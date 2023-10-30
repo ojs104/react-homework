a066e99059c6c632ab696d4375a146f4476085f9
ojs104 <kmdojs@gmail.com>
2023-10-30 13:26:14 +0900

    vue-webpage 10-30
    초반 setting

---
+++ src/components/ImgText.vue
@@ -0,0 +1,3 @@
+<template>
+    imageText
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Header from "@/components/Header.vue"
+import Footer from "@/components/Footer.vue"
+import Slider from "@/components/Slider.vue"
+import Image from "@/components/Image.vue"
+import ImgText from "@/components/ImgText.vue"
+import Card from "@/components/Card.vue"
+import Banner from "@/components/Banner.vue"
+import Text from "@/components/Text.vue"
+
+
+</script>
+<template>
+  <Header />
+  <main>
+    <Slider />
+    <Image />
+    <ImgText />
+    <Card />
+    <Banner />
+    <Text />
+  </main>
+  <Footer />
+</template>
