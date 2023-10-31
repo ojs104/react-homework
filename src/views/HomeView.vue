15de6030993c2189531e01f8a5ff85cbc415b931
ojs104 <kmdojs@gmail.com>
2023-10-31 11:15:51 +0900

    10-31

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,14 +11,16 @@ import Text from "@/components/Text.vue"
 
 </script>
 <template>
-  <Header />
-  <main>
-    <Slider />
-    <Image />
-    <ImgText />
-    <Card />
-    <Banner />
-    <Text />
-  </main>
-  <Footer />
+  <div>
+    <Header element="nexon" />
+    <main>
+      <Slider element="nexon" />
+      <Image element="section nexon" title="포트폴리오가 실력이다." />
+      <ImgText element="section nexon gray" title="이미지 텍스트 유형" />
+      <Card element="section nexon" title="웹스토리보이 강의" />
+      <Banner element="nexon" title="배너 영역" />
+      <Text element="section nexon" title="텍스트 유형01" />
+    </main>
+    <Footer element="next section gray" />
+  </div>
 </template>
